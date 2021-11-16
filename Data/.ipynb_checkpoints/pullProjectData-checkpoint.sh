35b1f5c67958bf88fa6ae74cbb16d8a51e2c0418
Andrew Blair <andrewblair@pcd-eh5q05d-lt.ucsf.edu>
2021-11-16 11:53:02 -0800

    Cleared cache

--- Data/.ipynb_checkpoints/pullProjectData-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/hg38.all.fa
-
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam
-
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/dm.json
-
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/vierstra_et_al.6mer-model.txt -o vierstra_et_al.6mer-model.txt
-
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/reads.bam.bai
