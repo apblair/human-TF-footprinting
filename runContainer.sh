34576797272050c087c097a0c1ddd52e246d4eb4
Andrew Blair <andrewblair@pcd-eh5q05d-lt.ucsf.edu>
2021-11-17 13:29:52 -0800

    Updated Dockerfile

--- runContainer.sh
+++ runContainer.sh
@@ -1,1 +1,1 @@
-docker run -p 8889:8888 -e JUPYTER_ENABLE_LAB=yes -v /Users/andrewblair/:/home/jovyan/work apblair/bmi206:latest 
+docker run -p 8889:8888 -e JUPYTER_ENABLE_LAB=yes -v /Users/andrewblair/:/home/jovyan apblair/bmi206:latest 
