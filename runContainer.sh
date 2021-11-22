678f6c26304d5909e896bb66e0ff8ae6a7838333
apblair <apblair@ucsc.edu>
2021-11-22 11:12:02 -0800

    runContainer.sh args working on local ubuntu 18.04

--- runContainer.sh
+++ runContainer.sh
@@ -1,1 +1,1 @@
-docker run -p 8889:8888 -e JUPYTER_ENABLE_LAB=yes -v /Users/andrewblair/:/home/jovyan apblair/bmi206:latest 
+docker run --user root -p 8889:8888 -e JUPYTER_ENABLE_LAB=yes -e CHOWN_HOME=yes -e CHOWN_HOME_OPTS='-R' -v /Users/andrewblair/:/home/jovyan/work apblair/bmi206:latest 
