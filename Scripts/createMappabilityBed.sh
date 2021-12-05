d71b676c186640e0a51a05dc7bdab46925ad2214
Andrew Blair <andrewblair@pcd-eh5q05d-lt.ucsf.edu>
2021-12-04 17:38:49 -0800

    Adding Scripts folder

---
+++ Scripts/createMappabilityBed.sh
@@ -0,0 +1,7 @@
+../Apps/./bigWigToBedGraph ../Data/wgEncodeCrgMapabilityAlign36mer.bigWig /dev/stdout
+| awk -v OFS="\t" '
+        $4 >= 0.5 { print $1, $2, $3, ".", ".", "+";
+        print $1, $2+36-1, $3+36-1, ".", ".", "-"; }
+    '
+| sort-bed --max-mem 16G -
+> ../Data/mappability.stranded.bed
