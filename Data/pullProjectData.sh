230b31ec83f1c6cb8ee855b316347c7411768baa
apblair <apblair@ucsc.edu>
2021-12-06 11:49:20 -0800

    CD19+ interval.all.bedgraph.gz added to pullProjectData

--- Data/pullProjectData.sh
+++ Data/pullProjectData.sh
@@ -1,12 +1,18 @@
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/hg38.all.fa
+# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/hg38.all.fa
 
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam
-
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/dm.json
-
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/vierstra_et_al.6mer-model.txt -o vierstra_et_al.6mer-model.txt
-
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam.bai
+# Personal Project
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/consensus.index/consensus_footprints_and_collapsed_motifs_hg38.bed.gz
 
 wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/encodeDCC/wgEncodeMapability/wgEncodeCrgMapabilityAlign36mer.bigWig
+
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD19+-DS17186/interval.all.bedgraph.gz 
+
+# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam
+
+# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam.bai
+
+# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/dm.json
+
+# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/vierstra_et_al.6mer-model.txt -o vierstra_et_al.6mer-model.txt
+
 
