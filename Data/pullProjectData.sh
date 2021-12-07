20e058376f47e3cff2b39f384608ccfe29594afd
apblair <andrew.blair@ucsf.edu>
2021-12-07 19:42:36 +0000

    pullProject pulls CD20+ extended data fig1 data

--- Data/pullProjectData.sh
+++ Data/pullProjectData.sh
@@ -1,11 +1,14 @@
-# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/hg38.all.fa
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/hg38.all.fa
 
 # Personal Project
-#wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/consensus.index/consensus_footprints_and_collapsed_motifs_hg38.bed.gz
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/consensus.index/consensus_footprints_and_collapsed_motifs_hg38.bed.gz
 
-#wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/encodeDCC/wgEncodeMapability/wgEncodeCrgMapabilityAlign36mer.bigWig
+wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/encodeDCC/wgEncodeMapability/wgEncodeCrgMapabilityAlign36mer.bigWig
 
-
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/interval.all.fps.0.0001.bed
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/interval.all.fps.0.001.bed
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/interval.all.fps.0.01.bed.gz
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/interval.all.fps.0.05.bed
 
 # Group Project
 wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam
