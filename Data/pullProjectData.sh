27576c22e774854823289ac3aa49ff89d7f292f3
apblair <andrew.blair@ucsf.edu>
2021-12-07 19:36:47 +0000

    Removed unnecessary files

--- Data/pullProjectData.sh
+++ Data/pullProjectData.sh
@@ -5,19 +5,11 @@
 
 #wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/encodeDCC/wgEncodeMapability/wgEncodeCrgMapabilityAlign36mer.bigWig
 
-#wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD19+-DS17186/interval.all.bedgraph.gz 
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD19+-DS17186/interval.all.obs.bw
 
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD19+-DS17186/interval.all.exp.bw
 
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD19+-DS17186/interval.all.fpr.bw
-
-# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam
-
-# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam.bai
-
-# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/dm.json
-
-# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/vierstra_et_al.6mer-model.txt -o vierstra_et_al.6mer-model.txt
+# Group Project
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/dm.json
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/vierstra_et_al.6mer-model.txt -o vierstra_et_al.6mer-model.txt
 
 
--- Scripts/convertInterval.all.bedgraph-toBED.sh
+++
@@ -1,2 +0,0 @@
-cat ../Data/interval.all.bedgraph | awk '{print $1 "\t" $2 "\t" $3}' > ../Data/interval.all.bed
-
