d16759e676eabd7c45fba92a1f6b25b577939211
apblair <andrew.blair@ucsf.edu>
2021-12-08 06:58:40 +0000

    Add liftOver and chain files

---
+++ Apps/pullApps.sh
@@ -0,0 +1,3 @@
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigToBedGraph -O bigWigToBedGraph-linux.x86_64
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigToWig -O bigWigToWig-linux.x86_64
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/liftOver -O bigWigToBedGraph-linux.x86_64
