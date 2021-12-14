e6c99b0dcd814dffe1e330e23626e682c2983647
apblair <andrew.blair@ucsf.edu>
2021-12-14 23:13:52 +0000

    Chr19 analysis

--- Data/pullProjectData.sh
+++ Data/pullProjectData.sh
@@ -1,4 +1,4 @@
-wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/hg38.all.fa
+# wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/hg38.all.fa
 
 # Personal Project
 wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/consensus.index/consensus_footprints_and_collapsed_motifs_hg38.bed.gz
@@ -12,7 +12,10 @@ wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.data
 wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz
 wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/interval.all.bedgraph.gz
 # Group Project
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/hg38.all.fa
+
 wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam
+wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/reads.bam.bai
 wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/per.dataset/CD20+-DS18208/dm.json
 wget https://resources.altius.org/~jvierstra/projects/footprinting.2020/vierstra_et_al.6mer-model.txt -o vierstra_et_al.6mer-model.txt
 
